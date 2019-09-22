fcdf70480450851bba528ecf80fc9b394fc7d97a
Allen Evans <allen@techinity.com>
2019-09-22 12:04:05 +0100

    feat: prevent overwriting existing env vars

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -2,6 +2,7 @@ import * as core from '@actions/core';
 
 jest.mock('@actions/core', () => ({
   exportVariable: jest.fn(),
+  getInput: jest.fn(),
   setFailed: jest.fn(),
 }));
 
@@ -36,19 +37,57 @@ describe('set-env', () => {
     expect(core.exportVariable).toHaveBeenCalledWith('C', '3');
   });
 
-  it('should report exceptions', () => {
-    (<jest.Mock>core.exportVariable).mockImplementation(() => {
-      throw new Error('FAIL');
-    });
+  it('should allow environment variables to be overwritten', () => {
     jest.spyOn(process, 'exit').mockImplementation((code?: number): never => {
       throw new Error(`${code}`);
     });
 
-    process.env.INPUT_TEST = 'test';
+    process.env.PROTECTED = 'do-not-change';
+    process.env.INPUT_PROTECTED = 'overwrite';
 
-    jest.isolateModules(() => expect(() => require('./main')).toThrowError('1'));
+    (<jest.Mock>core.getInput).mockImplementation(() => 'true');
 
-    expect(core.setFailed).toHaveBeenCalledTimes(1);
-    expect(core.setFailed).toHaveBeenCalledWith('FAIL');
+    jest.isolateModules(() => expect(() => require('./main')).not.toThrowError());
+
+    expect(core.exportVariable).toHaveBeenCalledWith('PROTECTED', 'overwrite');
+  });
+
+  describe('exceptions', () => {
+    beforeEach(() => {
+      jest.spyOn(process, 'exit').mockImplementation((code?: number): never => {
+        throw new Error(`${code}`);
+      });
+    });
+    it('should report exceptions', () => {
+      (<jest.Mock>core.exportVariable).mockImplementation(() => {
+        throw new Error('FAIL');
+      });
+      jest.spyOn(process, 'exit').mockImplementation((code?: number): never => {
+        throw new Error(`${code}`);
+      });
+
+      process.env.INPUT_TEST = 'test';
+
+      jest.isolateModules(() => expect(() => require('./main')).toThrowError());
+
+      expect(core.setFailed).toHaveBeenCalledTimes(1);
+      expect(core.setFailed).toHaveBeenCalledWith('FAIL');
+    });
+
+    it('should not allow existing environment variables to be overwritten', () => {
+      jest.spyOn(process, 'exit').mockImplementation((code?: number): never => {
+        throw new Error(`${code}`);
+      });
+
+      process.env.PROTECTED = 'do-not-change';
+      process.env.INPUT_PROTECTED = 'overwrite';
+
+      (<jest.Mock>core.getInput).mockImplementation(() => 'false');
+
+      jest.isolateModules(() => expect(() => require('./main')).toThrowError('1'));
+
+      expect(core.setFailed).toHaveBeenCalledTimes(1);
+      expect(core.setFailed).toHaveBeenCalledWith('Unable to overwrite existing environment variable PROTECTED');
+    });
   });
 });
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import * as core from '@actions/core';
 
+const PROTECTED_INPUT_VARS = ['INPUT_OVERWRITE'];
+
+const allowOverwrite = () => core.getInput('overwrite') === 'true';
+
+const setEnvironmentVariable = (key: string, value: string) => {
+  if (process.env[key] !== undefined && !allowOverwrite()) {
+    throw new Error(`Unable to overwrite existing environment variable ${key}`);
+  }
+  core.exportVariable(key, value);
+};
+
 (function run() {
   try {
     Object.keys(process.env)
-      .filter((key) => /^INPUT_/.test(key))
+      .filter((key) => /^INPUT_/.test(key) && !PROTECTED_INPUT_VARS.includes(key))
       .forEach((key) => {
-        core.exportVariable(key.replace(/^INPUT_/, ''), `${process.env[key]}`);
+        setEnvironmentVariable(key.replace(/^INPUT_/, ''), `${process.env[key]}`);
       });
   } catch (error) {
     core.setFailed(error.message);
